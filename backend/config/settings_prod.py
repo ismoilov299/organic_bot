1cc49127ffe87d270285fa6c2c95167dbad14587
ismoilov299 <regnad299@gmail.com>
2025-12-03 21:32:28 +0500

    Add CSRF_TRUSTED_ORIGINS for HTTP support

--- backend/config/settings_prod.py
+++ backend/config/settings_prod.py
@@ -10,12 +10,21 @@ ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(','
 
 # Security
 SECRET_KEY = os.environ.get('SECRET_KEY', SECRET_KEY)
+
+# HTTPS settings (only enable after SSL certificate is installed)
 SECURE_SSL_REDIRECT = os.environ.get('SECURE_SSL_REDIRECT', 'False') == 'True'
 SESSION_COOKIE_SECURE = os.environ.get('SESSION_COOKIE_SECURE', 'False') == 'True'
 CSRF_COOKIE_SECURE = os.environ.get('CSRF_COOKIE_SECURE', 'False') == 'True'
 SECURE_HSTS_SECONDS = int(os.environ.get('SECURE_HSTS_SECONDS', '0'))
 SECURE_HSTS_INCLUDE_SUBDOMAINS = os.environ.get('SECURE_HSTS_INCLUDE_SUBDOMAINS', 'False') == 'True'
 SECURE_HSTS_PRELOAD = os.environ.get('SECURE_HSTS_PRELOAD', 'False') == 'True'
+
+# CSRF trusted origins for HTTP (remove after HTTPS)
+CSRF_TRUSTED_ORIGINS = [
+    'http://45.138.159.52',
+    'http://organikbuyurtma.uz',
+    'http://www.organikbuyurtma.uz',
+]
 
 # Static files
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
